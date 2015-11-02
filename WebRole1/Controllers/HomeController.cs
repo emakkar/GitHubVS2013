586feaf59ba13251de10a3c80f582fd9496a1198
emakkar <emakkar@gmail.com>
2015-11-01 23:06:54 -0500

    minor change

--- WebRole1/Controllers/HomeController.cs
+++ WebRole1/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace WebRole1.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
